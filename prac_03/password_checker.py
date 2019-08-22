9ce9d7567bebc02bed79ff78de2d7897bb6fcc53
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2019-08-22 12:30:28 +1000

    Break code up into functions

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -2,7 +2,22 @@
 Dallas Marshall
 """
 MIN_LENGTH = 5
-password = input("Enter a password: ")
-while len(password) < MIN_LENGTH:
+
+
+def main():
+    password = get_password()
+    print_stars(password)
+
+
+def print_stars(password):
+    print("{}".format(len(password) * '*'))
+
+
+def get_password():
     password = input("Enter a password: ")
-print("{}".format(len(password) * '*'))
+    while len(password) < MIN_LENGTH:
+        password = input("Enter a password: ")
+    return password
+
+
+main()
