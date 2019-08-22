23a1fc219065cc4bafd9e96ba55b492ffa766817
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2019-08-22 12:49:52 +1000

    Change conversions to functions

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -8,15 +8,25 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def celsius_to_fahrenheit(temperature):
+    temperature_in_fahrenheit = temperature * 9.0 / 5 + 32
+    return temperature_in_fahrenheit
+
+
+def fahrenheit_to_celsius(temperature_fahrenheit):
+    temperature_in_celsius = 5 / 9 * (temperature_fahrenheit - 32)
+    return temperature_in_celsius
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
+        print("Result: {:.2f} F".format(celsius_to_fahrenheit(celsius)))
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("result: {:.2f}".format(celsius))
+        print("result: {:.2f}".format(fahrenheit_to_celsius(fahrenheit)))
     else:
         print("Invalid option")
     print(MENU)
