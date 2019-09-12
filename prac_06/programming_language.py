c41fe1cdc9a68920819a26223fe00e012500e043
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2019-09-12 12:04:00 +1000

    Add ProgrammingLanguage and languages from prac_06

---
+++ prac_06/programming_language.py
@@ -0,0 +1,22 @@
+"""
+Programming language class for prac_06 intermediate exercises.
+"""
+
+
+class ProgrammingLanguage:
+    """Represent a programming language object."""
+
+    def __init__(self, name="", typing="", reflection=True, year=0):
+        """Initialise a Programming Language instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Return a string representation of ProgrammingLanguage instance."""
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection,
+                                                                           self.year)
+
+    def is_dynamic(self):
+        return bool(self.typing.lower() == "dynamic")
