c1e4fcddea5b9011ce4ad196323cb73d92a1168c
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2019-10-30 23:13:42 +1000

    Add do from scratch exercise part 1 from prac_09

---
+++ prac_09/sort_files_1.py
@@ -0,0 +1,28 @@
+"""Do from scratch exercise part 1 for prac_09"""
+import os
+import shutil
+
+
+def main():
+    """Sort files into subdirectories for each extension."""
+    # Set working directory to new directory downloaded.
+    os.chdir("FilesToSort")
+
+    # Loop through each file in the (current) directory
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
+
+        # Get extension name
+        extension_name = filename.split('.')[-1]
+        # Try make a new directory with that name
+        try:
+            os.mkdir(extension_name)
+        except FileExistsError:
+            pass
+        # Move files from current location to new location
+        shutil.move(filename, extension_name)
+
+
+main()
