397924279731557ee3f9470b5625ab214e93e274
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2019-10-30 22:51:57 +1000

    Add bug fixes to program

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -17,7 +17,7 @@ def main():
     for filename in os.listdir('.'):
         if os.path.isdir(filename):
             continue
-
+        get_fixed_filename(filename)
         new_name = get_fixed_filename(filename)
         print("Renaming {} to {}".format(filename, new_name))
         os.rename(filename, new_name)
@@ -25,8 +25,35 @@ def main():
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
+    # Split filename from .txt identifier
+    separated_name = filename.split('.')
+    # Break separated_name into its characters in a list
+    separated_name_letters = [letter for letter in separated_name[0]]
+    # Ensure letters following a space are capitalised.
+    fixed_name = ""
+    for i, letter in enumerate(separated_name_letters):
+        if letter.isupper():
+            fixed_name += letter
+        elif separated_name_letters[i - 1] == " ":
+            fixed_name += letter.upper()
+        else:
+            fixed_name += letter
+    separated_name[0] = fixed_name
+    # TODO cannot figure out how to handle (_Hillsong)
+    new_name = ""
+    for i, character in enumerate(separated_name[0]):
+        # Remove spaces will be replaced with underscore in next step
+        if character == " ":
+            new_name += ""
+        elif character.isupper():
+            # Only add underscore before if the capital letter is not the first letter
+            if i > 0:
+                new_name += "_{}".format(character)
+            else:
+                new_name += character
+        else:
+            new_name += character
+    return new_name + ".txt"
 
 
 main()
